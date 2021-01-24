ad6f5c00cf80ff6920beb82a1aebccf25fded758
= <=>
2021-01-25 01:37:15 +0200

    Creating Auth with Firebase - Register and Login

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,13 @@
 import { createStore } from 'vuex'
 import coachesModule from './coaches/index.js'
 import requestsModule from './requests/index.js'
+import authModule from './auth/index.js'
 
 const store = createStore({
     modules: {
         coaches: coachesModule,
-        requests: requestsModule
-    },
-    state(){
-        return{
-            userId: 'c3'
-        }
-    },
-    getters: {
-        userId(state){
-           return state.userId
-        }
+        requests: requestsModule,
+        auth: authModule
     }
 })
 
