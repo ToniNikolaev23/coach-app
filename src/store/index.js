eaca951f9587512f2e6d3243a08764be1b4a486a
= <=>
2021-01-24 19:15:02 +0200

    Adding requests

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from 'vuex'
 import coachesModule from './coaches/index.js'
+import requestsModule from './requests/index.js'
 
 const store = createStore({
     modules: {
         coaches: coachesModule,
+        requests: requestsModule
     },
     state(){
         return{
---
+++ src/store/requests/getters.js
@@ -0,0 +1,11 @@
+export default {
+    getRequests(state, _, _2, rootGetters){
+        const coachId = rootGetters.userId
+
+
+        return state.requests.filter(req => req.coachId === coachId);
+    },
+    hasRequests(_,getters){
+        return getters.requests.length > 0 && getters.requests
+    }
+}
